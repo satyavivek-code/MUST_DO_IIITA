4fe9d74a465824d9cf7a680451319d1df6f4fe44
Satya Vivek Jaiswal <125399156+satyavivek-code@users.noreply.github.com>
2023-03-27 17:08:03 +0530

    Update Max Sum Contiguous Subarray.cpp

--- Max Sum Contiguous Subarray.cpp
+++ Max Sum Contiguous Subarray.cpp
@@ -20,3 +20,30 @@ int Solution::maxSubArray(const vector<int> &A) {
 
     return max;
 }
+
+/*
+vs code implementataion
+*/
+
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int maxSubarray(int ar[], int n){
+    int max = INT_MIN;
+    int end = 0;
+
+    for (int i = 0; i < n; i++){
+        end += ar[i];
+        if (end > max) max = end;
+        if (end < 0) end = 0;
+    }
+    return max;
+}
+
+int main(){
+    int arr[5] = {1, 2, 3, 4, -5};
+    int ans = maxSubarray(arr,5);
+
+    cout << ans; 
+}
