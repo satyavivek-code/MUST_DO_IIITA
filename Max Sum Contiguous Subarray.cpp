4a8debfdfe969af69722d45e3996a63bc2ace06c
Satya Vivek Jaiswal <125399156+satyavivek-code@users.noreply.github.com>
2023-03-27 17:00:49 +0530

    Create Max Sum Contiguous Subarray.cpp

---
+++ Max Sum Contiguous Subarray.cpp
@@ -0,0 +1,22 @@
+/*
+Kadane algorithim 
+*/
+
+
+int Solution::maxSubArray(const vector<int> &A) {
+    int max = INT_MIN;
+    int end = 0;
+    int n = A.size();
+    
+    for (int i = 0; i < n; i++){
+        end += A[i];
+        if (end > max){
+            max = end;
+        }
+        if (end < 0){
+            end = 0;
+        }
+    }
+
+    return max;
+}
